9f064304999ff14dfa27f6fcebf94e48025f03c5
IsraelRAlencar <israelalencarribeiro@gmail.com>
2024-07-10 09:20:00 -0400

    new - criando HeroController

---
+++ EFCore.WebAPI/Controllers/HeroController.cs
@@ -0,0 +1,40 @@
+﻿using EFCore.Domain;
+using EFCore.Repo;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCore.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeroController : Controller
+    {
+        private readonly HeroContext _context;
+        public HeroController(HeroContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult Get() {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex) {
+                return BadRequest($"Erro: {ex}");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Post(Hero value) {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+            }
+        }
+    }
+}
