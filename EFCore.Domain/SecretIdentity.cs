e3dc19c00056a0cc454f00deacaac3f56ee9f0f3
IsraelRAlencar <israelalencarribeiro@gmail.com>
2024-07-10 11:11:25 -0400

    fix - corrigindo SecretIdentity para suportar valores nulos no ORM

--- EFCore.Domain/SecretIdentity.cs
+++ EFCore.Domain/SecretIdentity.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string RealName { get; set; }
         public int HeroId { get; set; }
-        public Hero Hero { get; set; }
+        public Hero? Hero { get; set; }
     }
 }
