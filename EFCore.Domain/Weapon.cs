c60759f4eec90c810c2310646a625111c79f0831
IsraelRAlencar <israelalencarribeiro@gmail.com>
2024-07-10 11:08:10 -0400

    fix - corrigindo Weapons para aceitar valores nulos na parte da ORM

--- EFCore.Domain/Weapon.cs
+++ EFCore.Domain/Weapon.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public Hero Hero { get; set; }
+        public Hero? Hero { get; set; }
         public int HeroId { get; set; }
 
     }
