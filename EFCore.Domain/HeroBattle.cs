f44deab9846c028e8868cb98a3806192d51bae0f
IsraelRAlencar <israelalencarribeiro@gmail.com>
2024-07-10 11:12:52 -0400

    fix - corrigindo HeroBattle para aceitar valores nulos na ORM

--- EFCore.Domain/HeroBattle.cs
+++ EFCore.Domain/HeroBattle.cs
@@ -3,8 +3,8 @@
     public class HeroBattle
     {
         public int HeroId { get; set; }
-        public Hero Hero { get; set; }
+        public Hero? Hero { get; set; }
         public int BattleId { get; set; }
-        public Battle Battle { get; set; }
+        public Battle? Battle { get; set; }
     }
 }
